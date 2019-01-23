530bafa2fc5f4218e748f1e66177cfc6ad36eb1a
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2019-01-23 18:10:55 +0000

    original book setup back

--- document_original/README.md
+++
@@ -1,1 +0,0 @@
-This is David's PhD Dissertation! 
--- document_original/additionalSofware.R
+++
@@ -1,5 +0,0 @@
-#======================================================================================================
-# Additional Software Used
-#--------------------------------------------------
-# Word Counter
-devtools::install_github("benmarwick/wordcountaddin", type = "source", dependencies = TRUE)
