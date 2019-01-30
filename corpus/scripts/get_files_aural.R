a17a25e317159cf2a628d4d5b92c6387d521cd41
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2019-01-30 10:03:42 +0000

    start coding

--- corpus/scripts/get_files_aural.R
+++ corpus/scripts/get_files_aural.R
@@ -8,6 +8,8 @@ set.seed(42)
 stimuli <- sample(x = files, size = 30,replace = FALSE)
 cat(stimuli,sep = "\n")
 cat(stimuli,sep = " ")
+# Add Five Low 
+# 70, 59, 9, 88, 
 
 stimuli_short <- sample(x = stimuli, size = 15, replace = FALSE)
 stimuli_short
