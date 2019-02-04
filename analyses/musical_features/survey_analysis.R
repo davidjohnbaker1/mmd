75e2d77fcae995e76af80242a32e095bf933da27
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2019-02-04 14:17:45 +0000

    irr charts

--- analyses/musical_features/survey_analysis.R
+++ analyses/musical_features/survey_analysis.R
@@ -2,6 +2,7 @@
 # Analyze and Clean Survey Data
 #--------------------------------------------------
 library(tidyverse)
+library(irr)
 #--------------------------------------------------
 
 dictation_survey <- read_csv("aural_survey/Dictation_Survey_Responses.csv")
@@ -30,7 +31,20 @@ make_semester_category <- function(x){
   x
 }
 
+
 dictation_survey$What_Semester <- make_semester_category(dictation_survey$What_Semester)
+
+dictation_survey$What_Semester <- factor(dictation_survey$What_Semester, levels = c("First Semester", "Second Semester",
+                                                           "Third Semester","Fourth Semester", 
+                                                           "Advanced Undergradaute", "Graduate Studies"))
+
+
+View(dictation_survey)
+
+#======================================================================================================
+# Variable Checks
+table(unique(dictation_survey$years_teaching_aural))
+
 
 #======================================================================================================
 # Things to Begin to Plot
@@ -38,43 +52,102 @@ dictation_survey$What_Semester <- make_semester_category(dictation_survey$What_S
 # Basic Demographics of Survey Participants
 #--------------------------------------------------
 # Calculate IRR for Each Participant for
-
-#--------------------------------------------------
-# 
-
-
 #--------------------------------------------------
 # Difficulty for Average 2nd Year 
-dictation_survey %>%
-  filter(melody_number < 500) %>%
-  ggplot(aes(x = melody_number, y = Difficulty_2nd_Year, color = subject)) +
-  geom_point() + geom_smooth(method = "lm") + 
-  labs(title = "Difficulty of Melody Across Sample",
-       x = "Melody Number",
-       y = "Difficulty Rating") + 
-  scale_x_continuous(limits = c(0, 629)) + 
-  scale_y_continuous(limits = c(0,100))
+#--------------------------------------------------
+
+dictation_survey <- dictation_survey %>%
+  mutate(melody_rank = dense_rank(melody_number)) 
+
+sem <-function(x) {sd(x)/sqrt(length(x))}
 
 #--------------------------------------------------
-# Grammatical 
+# Second Year Plots
+
 dictation_survey %>%
-  ggplot(aes(x = melody_number, y = Grammar, color = subject)) +
-  geom_point() + geom_smooth(method = "lm") + 
-  labs(title = "How Grammatical",
-       x = "Melody Number",
-       y = "Grammar Rating") + 
-  scale_x_continuous(limits = c(0, 629)) + 
-  scale_y_continuous(limits = c(0,100))
+  select(melody_rank, Difficulty_2nd_Year, subject) %>%
+  group_by(melody_rank) %>%
+  mutate(difficulty_mean = mean(Difficulty_2nd_Year), 
+         difficulty_sem = sem(Difficulty_2nd_Year)) -> dictation_agreement_plot
+
+ggplot(dictation_agreement_plot, aes(x = melody_rank, y = difficulty_mean)) + 
+  geom_point() +
+  geom_errorbar(aes(ymin=difficulty_mean-difficulty_sem,
+                    ymax = difficulty_mean+difficulty_sem),
+                width = .1) +
+  labs(title = "Average Difficulty for 2nd Year of Melodies Across Sample",
+       subtitle = "Bars indicate Standard Error of the Mean",
+       x = "Melodies in Sample in Rank Order",
+       y = "Averaged Difficulty") +
+  theme_classic()
+
+#--------------------------------------------------
+# Grammar Plots 
+
+dictation_survey %>%
+  select(melody_rank, Grammar, subject) %>%
+  group_by(melody_rank) %>%
+  mutate(grammar_mean = mean(Grammar), 
+         grammar_sem = sem(Grammar)) -> dictation_agreement_plot_grammar
+
+ggplot(dictation_agreement_plot_grammar, aes(x = melody_rank, y = grammar_mean)) + 
+  geom_point() +
+  geom_errorbar(aes(ymin=grammar_mean-grammar_sem,
+                    ymax = grammar_mean+grammar_sem),
+                width = .1) +
+  labs(title = "Average Grammar Across Sample",
+       subtitle = "Bars indicate Standard Error of the Mean",
+       x = "Melodies in Sample in Rank Order",
+       y = "Averaged Grammar") +
+  theme_classic()
 
 
-# http://www.cookbook-r.com/Statistical_analysis/Inter-rater_reliability/
-# ICC
+
+#--------------------------------------------------
+# Calculate IRR + ICC 
+#--------------------------------------------------
+# Use two way model because both melodies and raters are selected as random effects 
+# Consitency 
+dictation_survey %>%
+  select(subject, melody_number, Difficulty_2nd_Year) %>%
+  spread(subject, Difficulty_2nd_Year) %>%
+  select(-melody_number) %>%
+  icc(model = "twoway",type = "consistency")
+  
+# ICC .715 
+
+# Cicchetti (1994)[16] gives the following often quoted guidelines for interpretation for kappa or ICC inter-rater agreement measures:
+#   
+#   Less than 0.40—poor.
+# Between 0.40 and 0.59—fair.
+# Between 0.60 and 0.74—good.
+# Between 0.75 and 1.00—excellent.
+# A different guideline is given by Koo and Li (2016)[17]:
+#   
+#   below 0.50: poor
+# between 0.50 and 0.75: moderate
+# between 0.75 and 0.90: good
+# above 0.90: excellent
+#--------------------------------------------------
+# Calculate ICC for Class Category 
+#--------------------------------------------------
+
 #--------------------------------------------------
 # Calculate All ICC 
 #--------------------------------------------------
 # Semester -- Use Ranked Data 
 
+dictation_survey %>%
+  select(subject, melody_number, What_Semester) %>%
+  spread(subject, What_Semester) %>%
+  select(-melody_number) %>%
+  kappam.fleiss()
 
+dictation_survey %>%
+  select(Difficulty_2nd_Year, Grammar, melody_number) %>%
+  group_by(melody_number) %>%
+  mutate(melody_cor = cor(Difficulty_2nd_Year, Grammar)) %>%
+  arrange(melody_cor)
 
 
 # Semester -- ranked data
