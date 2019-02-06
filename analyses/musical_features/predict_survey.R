7b367ccd8e797bf80a33264e1a119c44d403a407
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2019-02-06 12:07:22 +0000

    chapter 4 edits

--- analyses/musical_features/predict_survey.R
+++ analyses/musical_features/predict_survey.R
@@ -9,7 +9,7 @@
 library(tidyverse)
 library(GGally)
 library(psych)
-m
+library(corrr)
 #--------------------------------------------------
 # Data Import
 dictation_survey <- read_csv("aural_survey/Dictation_Survey_Responses.csv")
@@ -41,7 +41,77 @@ dictation_survey$stimulus <- remove_transpose(dictation_survey$stimulus)
 
 target %>% 
   left_join(fantastic_computations) -> melody_data 
-  
+
+#--------------------------------------------------
+# Plot Melody Against Various Features
+
+
+# Good Ones
+ggplot(melody_data, aes(x = p.entropy, y = mean_diff)) +
+  geom_point() + theme_minimal() +
+  labs(title = "Pitch Entropy", x = "Pitch Entropy", y = "Mean Difficulty") +
+  theme_classic()
+
+ggplot(melody_data, aes(x = tonalness, y = mean_diff)) +
+  geom_point() + theme_minimal() +
+  labs(title = "Tonalness", x = "Tonalness", y = "Mean Difficulty") +
+  theme_classic()
+
+ggplot(melody_data, aes(x = step.cont.loc.var, y = mean_diff)) +
+  geom_point() + theme_minimal() +
+  labs(title = "Stepwise Contour: Local Variation", x = "Stepwise Contour", y = "Mean Difficulty") +
+  theme_classic()
+
+ggplot(melody_data, aes(x = len, y = mean_diff)) +
+  geom_point() + theme_minimal() +
+  labs(title = "Melody Length", x = "Melody Length", y = "Mean Difficulty") +
+  theme_classic() 
+
+# Bad Ones
+
+ggplot(melody_data, aes(x = tonal.spike, y = mean_diff)) +
+  geom_point() + theme_minimal() +
+  labs(title = "Tonal Spike", x = "Tonal Spike", y = "Mean Difficulty") +
+  theme_classic()
+
+ggplot(melody_data, aes(x = step.cont.glob.dir	, y = mean_diff)) +
+  geom_point() + theme_minimal() +
+  labs(title = "Stepwise Contour: Global Direction??", x = "Stepwise Contour: Global Direction??", y = "Mean Difficulty") +
+  theme_classic()
+
+ggplot(melody_data, aes(x = mean.entropy, y = mean_diff)) +
+  geom_point() + theme_minimal() +
+  labs(title = "Mean Pitch Entropy", x = "Mean Pitch Entropy", y = "Mean Difficulty") +
+  theme_classic()
+
+ggplot(melody_data, aes(x = d.range, y = mean_diff)) +
+  geom_point() + theme_minimal() +
+  labs(title = "Durational Range", x = "Durational Range", y = "Mean Difficulty") +
+  theme_classic()
+#--------------------------------------------------
+
+
+melody_data %>%
+  ungroup(stimulus) %>%
+  select(mean_diff:step.cont.loc.var) %>%
+  correlate() %>%
+  shave() %>%
+  select(rowname, mean_diff, mean_gram) %>%
+  arrange(-mean_diff) %>%
+
+#--------------------------------------------------
+# FILE FOF DISSERATION
+melody_data %>%
+  ungroup(stimulus) %>%
+  select(mean_diff:step.cont.loc.var) %>%
+  correlate() %>%
+  shave() %>%
+  select(rowname, mean_diff, mean_gram) %>%
+  kableExtra::kable(digits = 2) -> difficulty_feature_data 
+
+write_rds(difficulty_feature_data,path = "analyses/musical_features/difficulty_feature_data_feb6.rds")
+#--------------------------------------------------  
+
 melody_data %>%
   select(stimulus,mean_diff, mean_gram, p.range,p.entropy,len, note.dens,tonalness) %>%
   pairs.panels()
