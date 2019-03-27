7a268f5a6f4acef858e74d99ae1ff7a0a7a55890
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2019-03-27 09:00:26 +0000

    cum plot

--- figures/cumPlot.R
+++ figures/cumPlot.R
@@ -57,11 +57,11 @@ anim_11 <-
   geom_segment(aes(xend = 10, yend = CumIC), linetype = 2, colour = 'grey') + 
   geom_point(size = 2) + 
   geom_segment(x = 1, xend = 10, y = 11 , yend = 11, linetype = 2) + # Dashed Line based on position
-#  geom_segment(x = 5.5, xend = 5.5, y = 0, yend = 11) + # Bold Line for Boundary
+  geom_segment(x = 3, xend = 3, y = 0, yend = 11) + # Bold Line for Boundary
   transition_reveal(Position) + 
   coord_cartesian(clip = 'off') + 
   labs(title = '"Low" Working Memory Capacity', y = 'Cumulative Information Content') + 
-  annotate(geom = "text", x = 1.5, y = 18.6, label = "WMC Limit") +
+  annotate(geom = "text", x = 1.5, y = 12, label = "WMC Limit") +
   annotate(geom = "text", x = 7, y = 2.5, label = "Segmentation Boundary \n for Selective Attention") +
   geom_label() +
   theme_minimal() +
